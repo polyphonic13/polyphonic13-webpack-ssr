852c5239144f2f9e4ce55c482c6bc4d744874873
Stephen Gilboy <sgilboy@meredith.com>
2017-12-12 15:39:01 -0800

    Randomize list items

--- services/todoservice.js
+++ services/todoservice.js
@@ -4,13 +4,18 @@ export default {
   getItems() {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
+        const todos = [
+          { id: ++id, avatar: 'http://i.pravatar.cc/60?cachebust=' + id, title: 'Build Campaign', subtitle: "<span class='grey--text text--darken-2'>Ali Connors</span> - Create Campaign for new client" },
+          { id: ++id, avatar: 'http://i.pravatar.cc/60?cachebust=' + id, title: 'Add List Feature', subtitle: "<span class='grey--text text--darken-2'>Alex Scott</span> - Make a ToDo List Feature" },
+          { id: ++id, avatar: 'http://i.pravatar.cc/60?cachebust=' + id, title: 'Remove Tech Debt', subtitle: "<span class='grey--text text--darken-2'>Sandra Adams</span> — Clean up the code" }
+        ];
         resolve([
           { header: 'Today' },
-          { id: ++id, avatar: 'http://i.pravatar.cc/60?cachebust=' + id, title: 'Build Campaign', subtitle: "<span class='grey--text text--darken-2'>Ali Connors</span> - Create Campaign for new client" },
+          todos[Math.floor(Math.random()*todos.length)],
           { divider: true, inset: true },
-          { id: ++id, avatar: 'http://i.pravatar.cc/60?cachebust=' + id, title: 'Add List Feature', subtitle: "<span class='grey--text text--darken-2'>Alex Scott</span> - Make a ToDo List Feature" },
+          todos[Math.floor(Math.random()*todos.length)],
           { divider: true, inset: true },
-          { id: ++id, avatar: 'http://i.pravatar.cc/60?cachebust=' + id, title: 'Remove Tech Debt', subtitle: "<span class='grey--text text--darken-2'>Sandra Adams</span> — Clean up the code" }
+          todos[Math.floor(Math.random()*todos.length)],
         ]);
       }, 600);
     });
